7975036bcb7bebbec4f0f35c36bab690904f128e
User <amalhidayah523@gmail.com>
2025-06-27 17:00:16 +0700

    initial commit

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ import os
 class Config:
     # Kunci rahasia untuk keamanan sesi Flask. Ganti dengan string acak yang SANGAT kuat!
     # Anda bisa generate di Python dengan: import os; os.urandom(24).hex()
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci_rahasia_anda_yang_sangat_kuat_sekali_ini_ubah_ini'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ubah_ini_dengan_kunci_rahasia_yang_sangat_kuat_dan_unik_anda'
     
-    # Konfigurasi koneksi database MySQL
-    # Ganti 'app_user' dan 'password_APLIKASI_MU_DISINI' dengan kredensial MySQL Anda
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://app_user:Amal191204@localhost/aplikasi_desa_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Konfigurasi koneksi database SQLite (FILE .db)
+    # Database akan dibuat di root folder aplikasi Anda (desa_app/site.db)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Direkomendasikan False untuk menghindari overhead
 
     # Konfigurasi untuk folder penyimpanan file yang diunggah
-    # app.root_path akan merujuk ke direktori root aplikasi (desa_app)
     UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # Batas ukuran file (16 Megabyte)
 
